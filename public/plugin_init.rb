368951887acbc2f838a0ed690829acd38156fedb
cfitz <chrisfitzpat@gmail.com>
2014-11-14 11:48:28 +0100

    Update plugin_init.rb

--- public/plugin_init.rb
+++ public/plugin_init.rb
@@ -1,2 +1,2 @@
 my_routes = [File.join(File.dirname(__FILE__), "routes.rb")]
-ArchivesSpace::Application.config.paths['config/routes'].concat(my_routes)
+ArchivesSpacePublic::Application.config.paths['config/routes'].concat(my_routes)
